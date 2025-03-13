67ea2f606963de60983da39530efddd96859e725
Felipe Barreto <felipebarreto148@gmail.com>
2025-03-12 23:12:38 -0300

    feat: create searcher component and update reset css.

---
+++ components/Searcher/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section class="absolute flex items-center gap-3 w-full sm:w-1/2 md:w-1/4 h-fit bg-white shadow rounded-md px-3 py-2 top-5 left-5">
+    <Icon name="streamline:magnifying-glass" />
+    <input type="text" class="size-full outline-none" placeholder="Pesquise por equipamentos aqui...">
+  </section>
+</template>
