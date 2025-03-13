ffa955354284d9943cb98560ebd4120bb0a59305
Felipe Barreto <felipebarreto148@gmail.com>
2025-03-12 22:48:08 -0300

    chore: install tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+// Plugins
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/eslint', '@nuxt/ui']
+  css: ["@/assets/styles/main.css"],
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 })
