2ef2189e8defc50bd3c152a956267a9731ff02ee
Felipe Barreto <felipebarreto148@gmail.com>
2025-03-13 00:28:16 -0300

    chore: add meta tags, change title and define Poppins as default font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,5 +18,27 @@ export default defineNuxtConfig({
     plugins: [
       tailwindcss()
     ]
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "pt-BR"
+      },
+      title: "Desafio | Felipe Barreto",
+      meta: [
+        { name: 'description', content: 'Bem-vindo(a) ao meu desafio para a Aiko!' },
+        { name: 'keywords', content: 'nuxt3, vue3, seo' },
+        { name: 'author', content: 'Felipe Barreto' },
+        { property: 'og:title', content: 'Desafio | Felipe Barreto' },
+        { property: 'og:description', content: 'Bem-vindo(a) ao meu desafio para a Aiko!' },
+        { property: 'og:image', content: '/_nuxt/img/aiko.png' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+        }
+      ]
+    }
   }
 })
