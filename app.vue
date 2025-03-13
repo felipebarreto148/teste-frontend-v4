6ac60cd12ac42d890ff55ad546b527e776fadf75
Felipe Barreto <felipebarreto148@gmail.com>
2025-03-13 15:40:36 -0300

    feat: add client only to map and change structure

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
-  <Map />
+  <main class="size-full relative flex flex-col">
+    <header class="flex items-center justify-between gap-3 p-3 z-999999">
+      <Searcher />
+      <Filters />
+    </header>
+    <ClientOnly>
+      <Map />
+    </ClientOnly>
+  </main>
 </template>
--- components/Map/index.vue
+++ components/Map/index.vue
@@ -6,7 +6,8 @@ import type { IEquipmentsWithMoreInfos } from "~/stores/equipments/equipments.sc
 import { useEquipmentsStore } from "~/stores/equipments";
 const useEquipments = useEquipmentsStore();
 
-const { equipmentsWithMoreInfos, selectedEquipment } = storeToRefs(useEquipments);
+const { equipmentsWithMoreInfos, selectedEquipment } =
+  storeToRefs(useEquipments);
 
 // Variables
 const detailsModalOpened = ref(false);
@@ -29,43 +30,38 @@ function openDetailsModal(equipment: IEquipmentsWithMoreInfos) {
 </script>
 
 <template>
-  <main class="size-full relative flex flex-col">
-    <header class="flex items-center justify-between gap-3 p-3 z-999999">
-      <Searcher />
-      <Filters />
-    </header>
-    <LMap
-      class="size-full absolute top-0 left-0"
-      :zoom="6"
-      :center="[-19.126536, -45.947756]"
-      :use-global-leaflet="false"
-      :zoom-control="false"
-      @ready="onMapReady"
-    >
-      <LTileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-        layer-type="base"
-        name="OpenStreetMap"
-      />
-      <!-- Controla o zoom -->
-      <LControlZoom position="bottomright" />
+  <LMap
+    ref="map"
+    class="size-full absolute top-0 left-0"
+    :zoom="6"
+    :center="[-19.126536, -45.947756]"
+    :use-global-leaflet="false"
+    :zoom-control="false"
+    @ready="onMapReady"
+  >
+    <LTileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      layer-type="base"
+      name="OpenStreetMap"
+    />
+    <!-- Controla o zoom -->
+    <LControlZoom position="bottomright" />
 
-      <!-- Renderiza os equipamentos no mapa -->
-      <LMarker
-        v-for="(equipment, index) in equipmentsWithMoreInfos"
-        :key="index"
-        :lat-lng="[equipment?.last_position?.lat, equipment?.last_position?.lon]"
-        @click="openDetailsModal(equipment)"
-      />
-    </LMap>
-    <BaseSlideover
-      v-model="detailsModalOpened"
-      :title="selectedEquipment?.name || 'Equipamento'"
-      subtitle="Visualize o histórico e o trajeto percorrido pelo equipamento."
-      @close="detailsModalOpened = !detailsModalOpened"
-    >
-      <EquipmentDetails />
-    </BaseSlideover>
-  </main>
+    <!-- Renderiza os equipamentos no mapa -->
+    <LMarker
+      v-for="(equipment, index) in equipmentsWithMoreInfos"
+      :key="index"
+      :lat-lng="[equipment?.last_position?.lat, equipment?.last_position?.lon]"
+      @click="openDetailsModal(equipment)"
+    />
+  </LMap>
+  <BaseSlideover
+    v-model="detailsModalOpened"
+    :title="selectedEquipment?.name || 'Equipamento'"
+    subtitle="Visualize o histórico e o trajeto percorrido pelo equipamento."
+    @close="detailsModalOpened = !detailsModalOpened"
+  >
+    <EquipmentDetails />
+  </BaseSlideover>
 </template>
